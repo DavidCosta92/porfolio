04434e32ad7f4e59c8ebb3575ba5690a30c584f4
David Costa <89800442+DavidCosta92@users.noreply.github.com>
2022-10-06 16:05:38 -0300

    detalles responsive

--- src/Herramientas/Herramientas.js
+++ src/Herramientas/Herramientas.js
@@ -16,27 +16,31 @@ const Herramientas = ()=>{
             </span>
             <span className="contenedorAprendiendo">
                 <p className="subtitulo">Aprendizaje constante</p>
-                <p>Actualmente estudio una Tecnicatura en desarrollo web y aplicaciones digitales
-                    donde estoy utilizando lenguajes y heramientas muy interesantes
-                </p>
-                <p className="softSkills">A la vez que fortalezco soft skills como</p>
-                <div>
-                    <span className="softContainer">
-                        <p>Trabajo en equipo</p>
-                        <p>Metodologias agiles</p>
-                        <p>Documentacion y requerimientos de proyecto</p>
-                    </span>
-                    <span className="toolsContainer">
+                <div className="contenedor">
+                    <div className="contenedorAprendizajes">
+                        <p>Actualmente estudio una Tecnicatura en desarrollo web y aplicaciones digitales
+                            donde estoy utilizando lenguajes y heramientas muy interesantes
+                        </p>
+                        <span className="toolsContainer">
                         <img src="./assets/icons/python.png" alt="Logo python"/>
-                        <img src="./assets/icons/sql.png" alt="Logo sql"/>
-                        <img src="./assets/icons/worpress.png" alt="Logo worpress"/>
-                        <img src="./assets/icons/team.png" alt="Logo Equipo de trabajo"/> 
-                        <img src="./assets/icons/scrum.png" alt="Logo Scrum"/>
-                        <img src="./assets/icons/ieee.png" alt="Logo IEEE"/>
-                    </span>
+                            <img src="./assets/icons/sql.png" alt="Logo sql"/>
+                            <img src="./assets/icons/worpress.png" alt="Logo worpress"/>
+                            <img src="./assets/icons/team.png" alt="Logo Equipo de trabajo"/> 
+                            <img src="./assets/icons/scrum.png" alt="Logo Scrum"/>
+                            <img src="./assets/icons/ieee.png" alt="Logo IEEE"/>
+                        </span>
+                    </div>
+                    <div className="contenedorAprendizajes">
+                        <p className="softSkills">Además la metodologia de estudio aborda los conocimientos practicos,
+                        de forma totalmente orientada al trabajo en equipos agiles y autogestionados, permitendo fortalecer soft skills como</p>
+                        <span className="softContainer">
+                            <p>Trabajo en equipos scrum</p>
+                            <p>Empatia y compañerismo</p>
+                            <p>Comunicacion asertiva</p>
+                            <p>Manejo de grupos y liderazgo</p>
+                        </span>
+                    </div>
                 </div>
-
-
             </span>
         </div>
     )
